9f6883a4a0fd9328468e1cf37e6ce209f22f985c
Tiến Phát <phatsuzai45@gmail.com>
2026-04-17 20:11:20 +0700

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,60 +1,34 @@
--- Load UI Library
+-- FIX LOAD
+repeat wait() until game.Players.LocalPlayer.Character
+
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Tạo cửa sổ
 local Window = Rayfield:CreateWindow({
-    Name = "Phat Hub | FPS Boost",
+    Name = "Phat Hub",
     LoadingTitle = "Loading...",
     LoadingSubtitle = "by Phat",
-    ConfigurationSaving = {
-        Enabled = false
-    }
+    ConfigurationSaving = {Enabled = false}
 })
 
--- TAB 1: FPS
-local TabFPS = Window:CreateTab("FPS", 4483362458)
+local Tab = Window:CreateTab("Main", 4483362458)
 
-TabFPS:CreateButton({
-    Name = "Boost FPS",
+Tab:CreateButton({
+    Name = "FPS BOOST",
     Callback = function()
         for i,v in pairs(game:GetDescendants()) do
             if v:IsA("Texture") or v:IsA("Decal") then
                 v:Destroy()
-            elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-                v.Enabled = false
-            elseif v:IsA("PointLight") then
+            elseif v:IsA("ParticleEmitter") then
                 v.Enabled = false
             end
         end
+
         game.Lighting.GlobalShadows = false
-        game.Lighting.FogEnd = 9e9
+
+        Rayfield:Notify({
+            Title = "Done",
+            Content = "Đã bật FPS Boost",
+            Duration = 3
+        })
     end
 })
-
--- TAB 2: Player
-local TabPlayer = Window:CreateTab("Player", 4483362458)
-
-TabPlayer:CreateSlider({
-    Name = "WalkSpeed",
-    Range = {16, 200},
-    Increment = 1,
-    CurrentValue = 16,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-    end
-})
-
-TabPlayer:CreateSlider({
-    Name = "JumpPower",
-    Range = {50, 200},
-    Increment = 1,
-    CurrentValue = 50,
-    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-    end
-})
-
--- TAB 3: Info
-local TabInfo = Window:CreateTab("Info", 4483362458)
-
-TabInfo:CreateLabel("Phat Hub v1")
